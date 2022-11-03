bbef67ecd54fdbfb9757fb486f7b3637f3e970d3
Jesse Giles <jesse.giles@telus.com>
2022-11-02 19:11:28 -0600

    Use a for loop to repeat a chrous line

---
+++ chorus-for.js
@@ -0,0 +1,10 @@
+let chorus = "But I'm betting on the right here, right now \n To hell with all the someday, somehow \n I've waited long enough \n Waited long enough (Dont need a miracle)";
+
+for (let repeat = 0; repeat <3; repeat++) {
+  if (repeat === 1){
+    chorus = chorus.replace("But I'm betting on the r", "R");
+  } else 
+  console.log(chorus);
+}
+
+console.log("You might think it's something spiritual \n But I don't need a ****ing miracle!");
